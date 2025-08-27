40d6987ef7d5f1eade6d71f91b6b02944e91f180
piyushmishra8008 <155667951+piyushmishra8008@users.noreply.github.com>
2025-08-27 15:35:40 +0530

    Time: 0 ms (100%), Space: 31 MB (5.97%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    void rotate(std::vector<int>& nums, int k) {
- vector<int>new1(nums.size());
- for(int i=0;i<nums.size();i++){
- new1[(i+k)%nums.size()]=nums[i];
- }
- nums=new1;
-}
+    void rotate(vector<int>& nums, int k) {
+        vector<int> arr;
+        int n =nums.size();
+         k = k % n;
+        for(int i=n-k;i<n;i++){
+            arr.push_back(nums[i]);
+        }
+        for(int i=0;i<n-k;i++){
+           arr.push_back(nums[i]);
+        }
+        nums=arr;
+    }
 };

dd642fd1bee70274811f49f8069e83eea2b2a198
piyushmishra8008 <155667951+piyushmishra8008@users.noreply.github.com>
2026-07-02 23:04:44 +0530

    Time: 0 ms (100%), Space: 24 MB (31.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,13 +4,13 @@ public:
         int n=intervals.size();
         vector<vector<int>>ans;
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<n;i++){
-            if(ans.empty()||intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
-            }else{
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=ans.back()[1]){
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-                ans.back()[0]=min(ans.back()[0],intervals[i][0]);
-            }
+            }else{
+            ans.push_back(intervals[i]);
+        }
         }
         return ans;
     }

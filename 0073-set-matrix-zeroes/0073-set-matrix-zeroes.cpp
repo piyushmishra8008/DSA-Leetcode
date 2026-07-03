629407ec88932c972f391a165548b3d6c93a5199
piyushmishra8008 <155667951+piyushmishra8008@users.noreply.github.com>
2026-07-03 18:31:51 +0530

    Time: 0 ms (100%), Space: 20.8 MB (50.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       int m=matrix.size();
-       int n=matrix[0].size();
-        vector<vector<int>>arr=matrix;
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]==0){
-                for(int k=0;k<m;k++){
-                    arr[k][j]=0;
-                }
-                for(int l=0;l<n;l++){
-                    arr[i][l]=0;
+        bool rowimpacted=false;
+        bool colimpacted=false;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int col=0;col<n;col++){
+            if(matrix[0][col]==0){
+               colimpacted=true; 
+            }
+        }
+        for(int row=0;row<m;row++){
+            if(matrix[row][0]==0){
+               rowimpacted=true; 
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-       }
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-        matrix[i][j]=arr[i][j];
+
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
         }
-       }
-      
+        if(colimpacted){
+            for(int col=0;col<n;col++){
+                matrix[0][col]=0;
+            }
+        }
+        
+        if(rowimpacted){
+            for(int row=0;row<m;row++){
+                matrix[row][0]=0;
+            }
+        }
+        
+
     }
 };

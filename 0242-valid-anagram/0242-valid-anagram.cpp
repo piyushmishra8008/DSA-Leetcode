5aa63ae8a85a61c9dc8418d5f72e40d1659a7156
piyushmishra8008 <155667951+piyushmishra8008@users.noreply.github.com>
2026-07-09 10:14:16 +0530

    Time: 0 ms (100%), Space: 10 MB (11.38%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       unordered_map<char,int>mp;
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        for(int j=0;j<t.length();j++){
+           if(mp.find(t[j])!=mp.end()){
+            mp[t[j]]--;
+           }else{
+            mp[t[j]]++;
+           }
+        }
+         for(auto it:mp){
+            if(it.second>=1){
+                return false;
+            }
+         }
+         return true;
+    }
+};
